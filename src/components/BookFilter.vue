b6699e979d76c3e6faad58d507373d0bc1313cff
Taly <talymoss@gmail.com>
2017-06-21 10:34:21 +0300

    first commit

---
+++ src/components/BookFilter.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="book-filter">
+        <h2>Books Filter</h2>
+        <input type="text" v-model="bookFilter.byText" placeholder="Filter By Name" >
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'book-list',
+        data() {
+            return {
+                 bookFilter: {byText: '', byMaxPrice: 1000 }
+            }
+        },
+        created() {
+        this.$watch('bookFilter.byText', function (newVal, oldVal) {
+            this.$emit('set-filter', this.bookFilter);
+        })
+    }  
+}
+</script>
---
+++ src/components/BookPreview.vue
@@ -0,0 +1,34 @@
+<template>
+  <li class="book-preview">
+             <h2>{{book.title}}</h2>
+             <img src="http://via.placeholder.com/250x150">
+             <h3>\${{book.price}}</h3>
+             <button @click.stop="editBook">&#9998;</button>
+             <button @click.stop="deleteBook">x</button>
+             <button @click.stop="addToCart">Add to Cart</button>
+         </li>
+</template>
+
+<script>
+export default {
+  name: 'book-preview',
+  props: ['book'],
+  methods: {
+        editBook(){
+            console.log('Request Edit');
+            this.$emit('edit');
+        },
+        deleteBook(){
+            console.log('Request Delete');
+            this.$emit('delete');
+        },
+        addToCart() {
+            this.$emit('add-to-cart');
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
